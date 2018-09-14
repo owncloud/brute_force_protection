862f6d71ea6297bffb5c8427f41b0b8afe7f128a
ownClouders <devops@owncloud.com>
2018-09-14 05:01:00 +0000

    [tx] updated from transifex

--- l10n/fr.js
+++ l10n/fr.js
@@ -4,8 +4,8 @@ OC.L10N.register(
     "Preferences are saved" : "Préférences sauvegardées",
     "Too many failed login attempts. Try again in %s minutes." : "Trop de tentatives de connexions échouées. Réessayez dans %s minutes.",
     "Brute Force Protection" : "Protection Brute Force",
-    "Ban after how many failed login attempts?" : "Bannir après combien de tentatives de connexions échoués?",
-    "Ban for how many seconds?" : "Bannir après combien de secondes?",
+    "Ban after how many failed login attempts?" : "échouéBannir après combien de tentatives de connexions échouées?",
+    "Ban for how many seconds?" : "Bannir pendant combien de secondes?",
     "Save settings" : "Sauvegarder les paramètres"
 },
 "nplurals=2; plural=(n > 1);");
