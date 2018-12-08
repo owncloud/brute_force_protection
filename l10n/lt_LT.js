14796cdf9915172124b8fb5d3c3d9846fecb066e
ownClouders <devops@owncloud.com>
2018-12-08 05:03:58 +0000

    [tx] updated from transifex

---
+++ l10n/lt_LT.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "brute_force_protection",
+    {
+    "Preferences are saved" : "Nustatymai išsaugoti",
+    "Inputs must be positive integers" : "Įvestis turi būti teigiami skaičiai",
+    "Too many failed login attempts. Try again in %s minutes." : "Per daug nepavykusių prisijungimų. Bandykite už %sminučių.",
+    "Brute Force Protection" : "Brutalios jėgos apsauga",
+    "Count failed login attempts over how many seconds?" : "Skaičiuoti nepavykusius prisijungimus per kiek sekundžių",
+    "Ban after how many failed login attempts?" : "Užblokuoti po kelių nepavykusių prisijungimų?",
+    "Ban for how many seconds?" : "Užblokuoti sekundėm?",
+    "Save settings" : "Išsaugoti nustatymus"
+},
+"nplurals=4; plural=(n % 10 == 1 && (n % 100 > 19 || n % 100 < 11) ? 0 : (n % 10 >= 2 && n % 10 <=9) && (n % 100 > 19 || n % 100 < 11) ? 1 : n % 1 != 0 ? 2: 3);");
