ab582ea386187bab5ad52f41b6c75b06b9199389
ownClouders <devops@owncloud.com>
2019-07-20 05:01:05 +0000

    [tx] updated from transifex

--- l10n/fr.js
+++ l10n/fr.js
@@ -2,6 +2,7 @@ OC.L10N.register(
     "brute_force_protection",
     {
     "Preferences are saved" : "Préférences sauvegardées",
+    "Inputs must be positive integers" : "Les champs doivent être des entiers positifs",
     "Too many failed login attempts. Try again in %s minutes." : "Trop de tentatives de connexions échouées. Réessayez dans %s minutes.",
     "Brute Force Protection" : "Protection Brute Force",
     "Ban after how many failed login attempts?" : "échouéBannir après combien de tentatives de connexions échouées?",
