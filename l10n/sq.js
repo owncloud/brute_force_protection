81da1ac756da05e176d9783ce87019c1c624024b
ownClouders <devops@owncloud.com>
2019-10-30 15:43:34 +0000

    [tx] updated from transifex

--- l10n/sq.js
+++ l10n/sq.js
@@ -3,7 +3,9 @@ OC.L10N.register(
     {
     "Preferences are saved" : "Parapëlqimet u ruajtën",
     "Inputs must be positive integers" : "Ato çka jepen duhet të jenë numra të plotë pozitivë",
-    "Too many failed login attempts. Try again in %s minutes." : "Shumë përpjekje të dështuara për hyrje. Riprovoni pas %s minutash?",
+    "Too many failed login attempts. Try again in %s." : "Shumë përpjekje të dështuara për hyrje. Riprovoni pas %s.",
+    "_ %n minute_::_ %n minutes_" : [" %n minute"," %n minutash"],
+    "_ %n hour_::_ %n hours_" : [" %n ore"," %n orësh"],
     "Brute Force Protection" : "Mbrojtje Nga Hyrjet Me Zor",
     "Count failed login attempts over how many seconds?" : "Të numërojë përpjekje të dështuara për hyrje tej sa sekondash? ",
     "Ban after how many failed login attempts?" : "Të dëbohet pas sa përpjekjesh të dështuara për hyrje?",
