4fbfcc04ed92d473985b66ee3b95f1921559eff9
Dominik Schmidt <domme@tomahawk-player.org>
2020-01-16 02:25:09 +0000

    [tx] updated from transifex

--- l10n/ar.js
+++ l10n/ar.js
@@ -1,13 +1,15 @@
 OC.L10N.register(
     "brute_force_protection",
     {
-    "Preferences are saved" : "التفضيلات يتم حفضها",
-    "Inputs must be positive integers" : "على المدخلات أن تكون أعداداً صحيحةً موجبة",
-    "Too many failed login attempts. Try again in %s minutes." : "هناك عدد كبير جدًا من محاولات تسجيل دخول فاشلة تمت. حاول مرة أخرى بعد %s دقيقة.",
-    "Brute Force Protection" : "حماية ضد هجمات القوة القسرية",
-    "Count failed login attempts over how many seconds?" : "عدد محاولات تسجيل الدخول الفاشلة تجاوز كم من عدد الثواني؟",
-    "Ban after how many failed login attempts?" : "الحظر بعد  كم من محاولات تسجيل الدخول الفاشلة؟",
-    "Ban for how many seconds?" : "الحضر عند كم من الثواني؟",
+    "Preferences are saved" : "يتم حفظ التفضيلات",
+    "Inputs must be positive integers" : "يجب أن تكون الإدخالات أعدادًا صحيحة موجبة",
+    "Too many failed login attempts. Try again in %s." : "عدد كبير للغاية من محاولات تسجيل الدخول الفاشلة. أعد المحاولة مرة أخرى في غضون %s.",
+    "_ %n minute_::_ %n minutes_" : [" %n دقيقة"," %n دقائق"," %n دقائق"," %n دقائق"," %n دقائق"," %n دقائق"],
+    "_ %n hour_::_ %n hours_" : [" %n ساعة"," %n ساعات"," %n ساعات"," %n ساعات"," %n ساعات"," %n ساعات"],
+    "Brute Force Protection" : "Brute Force Protection",
+    "Count failed login attempts over how many seconds?" : "عد محاولات تسجيل الدخول الفاشلة في كم ثانية؟",
+    "Ban after how many failed login attempts?" : "الحظر بعد كم محاولة تسجيل دخول فاشلة؟",
+    "Ban for how many seconds?" : "الحظر بعد كم ثانية؟",
     "Save settings" : "حفظ الإعدادات"
 },
 "nplurals=6; plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 && n%100<=99 ? 4 : 5;");
