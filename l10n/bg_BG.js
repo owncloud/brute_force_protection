278074304332526bc122e74d632fffaf53ec5d88
Dominik Schmidt <domme@tomahawk-player.org>
2020-02-09 02:26:27 +0000

    [tx] updated from transifex

---
+++ l10n/bg_BG.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "brute_force_protection",
+    {
+    "Preferences are saved" : "Предпочитанията се запазват",
+    "Inputs must be positive integers" : "Входните данни трябва да са положителни числа",
+    "Too many failed login attempts. Try again in %s." : "Твърде много неуспешни опити за влизане. Опитайте отново след %s.",
+    "Brute Force Protection" : "Защита от Brute Force",
+    "Count failed login attempts over how many seconds?" : "Твърде много неуспешни опити за влизане за колко секунди?",
+    "Ban after how many failed login attempts?" : "Забрани след колко неуспешни опита за влизане?",
+    "Ban for how many seconds?" : "Забрани за колко секунди?",
+    "Save settings" : "Запазване на настройките"
+},
+"nplurals=2; plural=(n != 1);");
