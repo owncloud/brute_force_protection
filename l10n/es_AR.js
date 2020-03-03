c366c08f7ffe0c1156ee91f4051ca3b8f7c41406
Dominik Schmidt <domme@tomahawk-player.org>
2020-03-03 03:38:18 +0000

    [tx] updated from transifex

---
+++ l10n/es_AR.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "brute_force_protection",
+    {
+    "Preferences are saved" : "Las preferencias fueron guardadas",
+    "Inputs must be positive integers" : "Los datos de entrada deben ser números enteros positivos",
+    "Too many failed login attempts. Try again in %s." : "Demasiados intentos de sesión erróneos. Reintente en %s.",
+    "_ %n minute_::_ %n minutes_" : ["%nminuto","%nminutos"],
+    "_ %n hour_::_ %n hours_" : ["%n hora","%n horas"],
+    "Brute Force Protection" : "Protección ante fuerza bruta",
+    "Ban after how many failed login attempts?" : "¿Bloquear luego de cuántos intentos de sesión?",
+    "Ban for how many seconds?" : "¿Bloquear por cuántos segundos?",
+    "Save settings" : "Guardar configuración"
+},
+"nplurals=2; plural=(n != 1);");
