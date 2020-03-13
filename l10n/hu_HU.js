3dbc8941a742e6e927ef80c2dac28c5c57ac511f
Thomas Müller <thomas.mueller@tmit.eu>
2020-03-13 16:37:59 +0000

    [tx] updated from transifex

---
+++ l10n/hu_HU.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "brute_force_protection",
+    {
+    "Preferences are saved" : "Beállításai mentve",
+    "Inputs must be positive integers" : "Az értékeknek pozitív egész számoknak kell lenniük",
+    "Too many failed login attempts. Try again in %s." : "Túl sok hibás bejelentkezési kísérlet. Próbálja újra %s múlva.",
+    "Brute Force Protection" : "Brute force támadás elleni védelem",
+    "Count failed login attempts over how many seconds?" : "Milyen időtávon keresztül számlálja a bejelentkezési kísérleteket?",
+    "Ban after how many failed login attempts?" : "Hány bejelentkezési kísérletet követően tiltson le?",
+    "Ban for how many seconds?" : "Hány másodpercre tiltson le?",
+    "Save settings" : "Beállítások mentése"
+},
+"nplurals=2; plural=(n != 1);");
