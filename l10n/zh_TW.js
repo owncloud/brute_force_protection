1c2593e63d9b99e46a358fea8f897a9664029bcc
Thomas Müller <thomas.mueller@tmit.eu>
2020-09-30 00:07:29 +0000

    [tx] updated from transifex

---
+++ l10n/zh_TW.js
@@ -0,0 +1,16 @@
+OC.L10N.register(
+    "brute_force_protection",
+    {
+    "Preferences are saved" : "首選項已儲存的",
+    "Inputs must be positive integers" : "輸入必須為正整數",
+    "Too many failed login attempts. Try again in %s." : "登錄嘗試失敗的次數過多。 再試一次%s",
+    "Too many failed attempts. Try again in %s." : "太多次失敗的嘗試。 再試一次%s",
+    "_ %n minute_::_ %n minutes_" : ["%n分鐘"],
+    "_ %n hour_::_ %n hours_" : ["%n小時"],
+    "Brute Force Protection" : "蠻力保護",
+    "Count failed attempts over how many seconds?" : "計算失敗嘗試的時間有多少秒？",
+    "Ban after how many failed attempts?" : "禁止幾次失敗嘗試之後？",
+    "Ban for how many seconds?" : "禁止多少秒？",
+    "Save settings" : "儲存設定"
+},
+"nplurals=1; plural=0;");
