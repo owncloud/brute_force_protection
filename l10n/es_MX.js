4639f353b06974b8c72c3ff95a3f4bd0bdbd4455
Thomas Müller <thomas.mueller@tmit.eu>
2020-10-23 06:27:35 +0000

    [tx] updated from transifex

--- l10n/es_MX.js
+++ l10n/es_MX.js
@@ -9,7 +9,7 @@ OC.L10N.register(
     "_ %n hour_::_ %n hours_" : [" %n hora"," %n horas"],
     "Brute Force Protection" : "Protección de fuerza bruta",
     "Count failed attempts over how many seconds?" : "Contar intentos fallidos a través de cuántos segundos?",
-    "Ban after how many failed attempts?" : "Aplican Ban después de cúantos intentos fallidos?",
+    "Ban after how many failed attempts?" : "Aplican Ban después de cuántos intentos fallidos?",
     "Ban for how many seconds?" : "¿Baneo por cuantos segundos?",
     "Save settings" : "Guardar configuraciones"
 },
