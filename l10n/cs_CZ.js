3c2b3b0742d8330e99f8e16367c89eec36afa8b7
Benedikt Kulmann <benedikt@kulmann.biz>
2021-03-05 00:06:15 +0000

    [tx] updated from transifex

--- l10n/cs_CZ.js
+++ l10n/cs_CZ.js
@@ -3,10 +3,13 @@ OC.L10N.register(
     {
     "Preferences are saved" : "Předvolby jsou uloženy",
     "Inputs must be positive integers" : "Vstup musí být kladné číslo",
-    "Too many failed login attempts. Try again in %s minutes." : "Příliš mnoho neúspěšných pokusů o přihlášení. Můžete opakovat po %s minutách.",
+    "Too many failed login attempts. Try again in %s." : "Příliš mnoho neúspěšných pokusů o přihlášení. Můžete opakovat po %s .",
+    "Too many failed attempts. Try again in %s." : "Příliš mnoho neúspěšných pokusů. Můžete opakovat po %s .",
+    "_ %n minute_::_ %n minutes_" : [" %n minuta"," %n minut","%n minut"," %n minut"],
+    "_ %n hour_::_ %n hours_" : [" %n hodina"," %n hodin"," %n hodin"," %n hodin"],
     "Brute Force Protection" : "Ochrana proti útok hrubou silou",
-    "Count failed login attempts over how many seconds?" : "Počet nezdařených pokusů o přihlášení v kolika sekundách?",
-    "Ban after how many failed login attempts?" : "Ban po kolika neúspěšných pokusech o přihlášení?",
+    "Count failed attempts over how many seconds?" : "Počet nezdařených pokusů v kolika sekundách?",
+    "Ban after how many failed attempts?" : "Ban po kolika neúspěšných pokusech?",
     "Ban for how many seconds?" : "Ban na dobu v sekundách?",
     "Save settings" : "Ulož nastavení"
 },
