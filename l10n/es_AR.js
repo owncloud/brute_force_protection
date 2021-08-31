6e3168d34b5ce408bc6b3d64433278f7fe767a19
Michael Barz <mbarz@owncloud.com>
2021-08-31 00:07:15 +0000

    [tx] updated from transifex

--- l10n/es_AR.js
+++ l10n/es_AR.js
@@ -4,10 +4,12 @@ OC.L10N.register(
     "Preferences are saved" : "Las preferencias fueron guardadas",
     "Inputs must be positive integers" : "Los datos de entrada deben ser números enteros positivos",
     "Too many failed login attempts. Try again in %s." : "Demasiados intentos de sesión erróneos. Reintente en %s.",
+    "Too many failed attempts. Try again in %s." : "Demasiados intentos fallidos. Intente nuevamente en 1%s.",
     "_ %n minute_::_ %n minutes_" : ["%nminuto","%nminutos"],
     "_ %n hour_::_ %n hours_" : ["%n hora","%n horas"],
     "Brute Force Protection" : "Protección ante fuerza bruta",
-    "Ban after how many failed login attempts?" : "¿Bloquear luego de cuántos intentos de sesión?",
+    "Count failed attempts over how many seconds?" : "¿Contar los intentos de inicio de sesión fallidos durante cuántos segundos?",
+    "Ban after how many failed attempts?" : "¿Banear después de cuántos intentos fallidos de inicio de sesión?",
     "Ban for how many seconds?" : "¿Bloquear por cuántos segundos?",
     "Save settings" : "Guardar configuración"
 },
