d3b6b200ec7cbd25f67f2110eaafce2af36f91d9
Lukas Hirt <lhirt@owncloud.com>
2021-09-21 00:05:34 +0000

    [tx] updated from transifex

--- l10n/el.js
+++ l10n/el.js
@@ -5,10 +5,11 @@ OC.L10N.register(
     "Inputs must be positive integers" : "Οι είσοδοι πρέπει να είναι θετικοί ακέραιοι αριθμοί",
     "Too many failed login attempts. Try again in %s." : "Πάρα πολλές αποτυχημένες προσπάθειες σύνδεσης. Δοκιμάστε ξανά σε %s.",
     "Too many failed attempts. Try again in %s." : "Πάρα πολλές αποτυχημένες προσπάθειες. Δοκιμάστε ξανά σε %s.",
-    "_ %n minute_::_ %n minutes_" : ["%n λεπτό","%n λεπτά"],
-    "_ %n hour_::_ %n hours_" : ["%n ώρα","%n ώρες"],
+    "_ %n minute_::_ %n minutes_" : [" %n λεπτό"," %n λεπτά"],
+    "_ %n hour_::_ %n hours_" : [" %n ώρα"," %n ώρες"],
     "Brute Force Protection" : "Προστασία από Brute Force",
     "Count failed attempts over how many seconds?" : "Καταμέτρηση αποτυχημένων προσπαθειών για πόσα δευτερόλεπτα;",
+    "Ban after how many failed attempts?" : "Μετά από πόσες αποτυχημένες προσπάθειες να γίνεται αποκλεισμός;",
     "Ban for how many seconds?" : "Για πόσα δευτερόλεπτα ο αποκλεισμός;",
     "Save settings" : "Αποθήκευση ρυθμίσεων"
 },
