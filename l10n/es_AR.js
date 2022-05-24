8af38fc7b63172422bd6ed2233ba88bf9e43c7fa
ownClouders <devops@owncloud.com>
2022-05-24 00:39:22 +0000

    [tx] updated from transifex

--- l10n/es_AR.js
+++ l10n/es_AR.js
@@ -5,12 +5,12 @@ OC.L10N.register(
     "Inputs must be positive integers" : "Los datos de entrada deben ser números enteros positivos",
     "Too many failed login attempts. Try again in %s." : "Demasiados intentos de sesión erróneos. Reintente en %s.",
     "Too many failed attempts. Try again in %s." : "Demasiados intentos fallidos. Intente nuevamente en 1%s.",
-    "_ %n minute_::_ %n minutes_" : ["%nminuto","%nminutos"],
-    "_ %n hour_::_ %n hours_" : ["%n hora","%n horas"],
+    "_ %n minute_::_ %n minutes_" : ["%nminuto","%nminutos","%nminutos"],
+    "_ %n hour_::_ %n hours_" : ["%n hora","%n horas","%n horas"],
     "Brute Force Protection" : "Protección ante fuerza bruta",
     "Count failed attempts over how many seconds?" : "¿Contar los intentos de inicio de sesión fallidos durante cuántos segundos?",
     "Ban after how many failed attempts?" : "¿Banear después de cuántos intentos fallidos de inicio de sesión?",
     "Ban for how many seconds?" : "¿Bloquear por cuántos segundos?",
     "Save settings" : "Guardar configuración"
 },
-"nplurals=2; plural=(n != 1);");
+"nplurals=3; plural=n == 1 ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
--- l10n/it.js
+++ l10n/it.js
@@ -5,12 +5,12 @@ OC.L10N.register(
     "Inputs must be positive integers" : "Inserire solo numeri interi positivi",
     "Too many failed login attempts. Try again in %s." : "Troppi tentativi di login falliti. Riprova tra %s.",
     "Too many failed attempts. Try again in %s." : "Troppi tentativi falliti. Riprova tra %s.",
-    "_ %n minute_::_ %n minutes_" : ["%n minuto","%n minuti"],
-    "_ %n hour_::_ %n hours_" : ["%n ora","%n ore"],
+    "_ %n minute_::_ %n minutes_" : ["%n minuto","%n minuti","%n minuti"],
+    "_ %n hour_::_ %n hours_" : ["%n ora","%n ore","%n ore"],
     "Brute Force Protection" : "Protezione da attacchi Brute Force",
     "Count failed attempts over how many seconds?" : "Contare i tentativi falliti ogni quanti secondi?",
     "Ban after how many failed attempts?" : "Dopo quanti tentativi falliti va bannato l'utente?",
     "Ban for how many seconds?" : "Per quanto tempo in secondi va bannato l'utente?",
     "Save settings" : "Salva le impostazioni"
 },
-"nplurals=2; plural=(n != 1);");
+"nplurals=3; plural=n == 1 ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
