30fc0b7798d204dffec44b501ba78a6234e927bd
ownClouders <devops@owncloud.com>
2022-12-15 01:10:15 +0000

    [tx] updated from transifex

--- l10n/id.js
+++ l10n/id.js
@@ -3,10 +3,13 @@ OC.L10N.register(
     {
     "Preferences are saved" : "Preferensi disimpan",
     "Inputs must be positive integers" : "Input harus bilangan bulat positif",
-    "Too many failed login attempts. Try again in %s minutes." : "Terlalu banyak upaya masuk yang gagal. Coba lagi dalam %s menit.",
+    "Too many failed login attempts. Try again in %s." : "Terlalu banyak upaya masuk yang gagal. Coba lagi dalam %s.",
+    "Too many failed attempts. Try again in %s." : "Terlalu banyak upaya yang gagal. Coba lagi dalam %s.",
+    "_ %n minute_::_ %n minutes_" : ["%n menit"],
+    "_ %n hour_::_ %n hours_" : ["%n jam"],
     "Brute Force Protection" : "Perlindungan Brute Force",
-    "Count failed login attempts over how many seconds?" : "Menghitung usaha masuk gagal selama berapa detik?",
-    "Ban after how many failed login attempts?" : "Blokir setelah seberapa banyak upaya masuk yang gagal?",
+    "Count failed attempts over how many seconds?" : "Hitung upaya yang gagal selama berapa detik?",
+    "Ban after how many failed attempts?" : "Blokir setelah seberapa banyak upaya masuk yang gagal?",
     "Ban for how many seconds?" : "Blokir selama berapa detik?",
     "Save settings" : "Simpan pengaturan"
 },
