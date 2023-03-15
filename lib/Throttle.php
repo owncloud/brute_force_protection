67591869da2f81bd53c51a9889d8f37a34af1724
Jürgen Weigert <jnweiger@gmail.com>
2023-03-15 12:26:03 +0100

    Update lib/Throttle.php
    whitespace

--- lib/Throttle.php
+++ lib/Throttle.php
@@ -87,7 +87,7 @@ class Throttle {
 		}
 		$thresholdTime = $lastAttempt - $this->config->getBruteForceProtectionTimeThreshold();
 		if ($this->loginAttemptMapper->getFailedLoginCountForUidIpCombination($uid, $ip, $thresholdTime) >=
-			$this->config->getBruteForceProtectionFailTolerance() -1 ) {
+			$this->config->getBruteForceProtectionFailTolerance() - 1) {
 			throw new LoginException(
 				$this->l->t(
 					"Too many failed login attempts. Try again in %s.",
