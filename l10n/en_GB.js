30ddb57901375f858bc23d2c4fc90a2750eca81f
ownClouders <devops@owncloud.com>
2023-04-29 00:47:17 +0000

    [tx] updated from transifex

--- l10n/en_GB.js
+++ l10n/en_GB.js
@@ -4,9 +4,11 @@ OC.L10N.register(
     "Preferences are saved" : "Preferences are saved",
     "Inputs must be positive integers" : "Inputs must be positive integers",
     "Too many failed login attempts. Try again in %s." : "Too many failed login attempts. Try again in %s.",
+    "Too many failed attempts. Try again in %s." : "Too many failed attempts. Try again in %s.",
     "_ %n minute_::_ %n minutes_" : [" %n minute"," %n minutes"],
     "_ %n hour_::_ %n hours_" : [" %n hour"," %n hours"],
     "Brute Force Protection" : "Brute Force Protection",
+    "Count failed attempts over how many seconds?" : "Count failed attempts over how many seconds?",
     "Ban after how many failed attempts?" : "Ban after how many failed attempts?",
     "Ban for how many seconds?" : "Ban for how many seconds?",
     "Save settings" : "Save settings"
