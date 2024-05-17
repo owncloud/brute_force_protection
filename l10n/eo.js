d00a353b6b92ba1a56f4ec4f89dcda2fcfeaae85
ownClouders <devops@owncloud.com>
2024-05-17 00:09:13 +0000

    [tx] updated from transifex

---
+++ l10n/eo.js
@@ -0,0 +1,16 @@
+OC.L10N.register(
+    "brute_force_protection",
+    {
+    "Preferences are saved" : "La preferoj konserviĝis",
+    "Inputs must be positive integers" : "Enigoj devas pozlitivajn entjerojn",
+    "Too many failed login attempts. Try again in %s." : "Tro pli malsukcesaj ensalutaj provoj. Provu ankoraŭ en %s",
+    "Too many failed attempts. Try again in %s." : "Tro pli malsukcesaj provoj. Provu ankoraŭ en %s",
+    "_ %n minute_::_ %n minutes_" : ["%n minuto","%n minutoj"],
+    "_ %n hour_::_ %n hours_" : ["%n horo","%n horoj"],
+    "Brute Force Protection" : "Brutforta Protektado",
+    "Count failed attempts over how many seconds?" : "Nombri malsukcesajn provojn trans kiom da sekundoj?",
+    "Ban after how many failed attempts?" : "Malpermesi post kiom da malsukcesaj provoj?",
+    "Ban for how many seconds?" : "Malpermesi por kiom da sekundoj?",
+    "Save settings" : "Konservi agordo"
+},
+"nplurals=2; plural=(n != 1);");
