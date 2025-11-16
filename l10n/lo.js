365dd03e7eee1b78522a12490dd5227cc6b5d357
ownClouders <devops@owncloud.com>
2025-11-16 00:30:15 +0000

    [tx] updated from transifex

---
+++ l10n/lo.js
@@ -0,0 +1,16 @@
+OC.L10N.register(
+    "brute_force_protection",
+    {
+    "Preferences are saved" : "ບັນທຶກການຕັ້ງຄ່າແລ້ວ",
+    "Inputs must be positive integers" : "ຄ່າທີ່ປ້ອນຕ້ອງເປັນຈຳນວນຖ້ວນບວກ",
+    "Too many failed login attempts. Try again in %s." : "ການພະຍາຍາມເຂົ້າສູ່ລະບົບລົ້ມເຫຼວຫຼາຍເກີນໄປ. ກະລຸນາລອງໃໝ່ໃນອີກ %s.",
+    "Too many failed attempts. Try again in %s." : "ການພະຍາຍາມລົ້ມເຫຼວຫຼາຍເກີນໄປ. ກະລຸນາລອງໃໝ່ໃນອີກ %s.",
+    "_ %n minute_::_ %n minutes_" : [" %n ນາທີ"],
+    "_ %n hour_::_ %n hours_" : [" %n ຊົ່ວໂມງ"],
+    "Brute Force Protection" : "ການປ້ອງກັນການໂຈມຕີແບບ Brute Force",
+    "Count failed attempts over how many seconds?" : "ນັບການພະຍາຍາມທີ່ລົ້ມເຫຼວພາຍໃນຈັກວິນາທີ?",
+    "Ban after how many failed attempts?" : "ບລັອກຫຼັງຈາກພະຍາຍາມລົ້ມເຫຼວຈັກຄັ້ງ?",
+    "Ban for how many seconds?" : "ບລັອກເປັນເວລາຈັກວິນາທີ?",
+    "Save settings" : "ບັນທຶກການຕັ້ງຄ່າ"
+},
+"nplurals=1; plural=0;");
